918c2eff34156335061d0d878bd3fbecdbde1ee2
BioXTC <106894209+BioXTC@users.noreply.github.com>
2024-04-21 01:28:33 -0400

    partial elms easter egg mode implemented, misc

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,9 +1,25 @@
+//https://www.w3schools.com/howto/howto_js_toggle_dark_mode.asp
 
-function myFunction() {
-    var x = document.getElementById("topNavbar");
-    if (x.className === "top_navbar") {
-      x.className += " responsive";
-    } else {
-      x.className = "top_navbar";
-    }
+function myFunctiona() {
+    var element = document.body;
+    element.classList.toggle("dark-mode");
   }
+
+function elmsMode() {
+    var element1 = document.querySelector(".navbar");
+    element1.classList.toggle("elms-mode");
+    var element2 = document.querySelector("body");
+    element2.classList.toggle("elms-mode");
+    var element3 = document.querySelector("#row-1_column-1");
+    element3.classList.toggle("elms-mode");
+    var element4 = document.querySelector("#row-2_column-1");
+    element4.classList.toggle("elms-mode");
+    var element5 = document.querySelector(".background-blue");
+    element5.classList.toggle("elms-mode");
+
+    var element3 = document.querySelector(".divider-img-410291");
+    element3.classList.toggle("divider-img-410291-white");
+ 
+  }
+
+ 
